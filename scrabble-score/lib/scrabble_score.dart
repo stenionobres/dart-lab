18c1027bc555f19f08cfcb50b211fd5d875e8354
Stenio Nobres <stenio.nobres@gmail.com>
2018-08-21 19:27:40 -0300

    Adjust code according exercism mentor feedback
    Make a scrabble_core.dart a file with a single function like a test class interface expects. In addiction the _pointsByCaracter map was changed to a const structure.

--- scrabble-score/lib/scrabble_score.dart
+++ scrabble-score/lib/scrabble_score.dart
@@ -1,44 +1,41 @@
-class ScrabbleScore{
-  var _pointsByCaracter = new Map<String, int>();
-
-  ScrabbleScore(){
-    _pointsByCaracter[''] = 0;
-    _pointsByCaracter['a'] = 1;
-    _pointsByCaracter['b'] = 3;
-    _pointsByCaracter['c'] = 3;
-    _pointsByCaracter['d'] = 2;
-    _pointsByCaracter['e'] = 1;
-    _pointsByCaracter['f'] = 4;
-    _pointsByCaracter['g'] = 2;
-    _pointsByCaracter['h'] = 4;
-    _pointsByCaracter['i'] = 1;
-    _pointsByCaracter['j'] = 8;
-    _pointsByCaracter['k'] = 5;
-    _pointsByCaracter['l'] = 1;
-    _pointsByCaracter['m'] = 3;
-    _pointsByCaracter['n'] = 1;
-    _pointsByCaracter['o'] = 1;
-    _pointsByCaracter['p'] = 3;
-    _pointsByCaracter['q'] = 10;
-    _pointsByCaracter['r'] = 1;
-    _pointsByCaracter['s'] = 1;
-    _pointsByCaracter['t'] = 1;
-    _pointsByCaracter['u'] = 1;
-    _pointsByCaracter['v'] = 4;
-    _pointsByCaracter['w'] = 4;
-    _pointsByCaracter['x'] = 8;
-    _pointsByCaracter['y'] = 4;
-    _pointsByCaracter['z'] = 10;
-  }
+  const Map<String, int> _pointsByCaracter = {
+    ''  : 0,
+    'a' : 1,
+    'b' : 3,
+    'c' : 3,
+    'd' : 2,
+    'e' : 1,
+    'f' : 4,
+    'g' : 2,
+    'h' : 4,
+    'i' : 1,
+    'j' : 8,
+    'k' : 5,
+    'l' : 1,
+    'm' : 3,
+    'n' : 1,
+    'o' : 1,
+    'p' : 3,
+    'q' : 10,
+    'r' : 1,
+    's' : 1,
+    't' : 1,
+    'u' : 1,
+    'v' : 4,
+    'w' : 4,
+    'x' : 8,
+    'y' : 4,
+    'z' : 10
+  };
 
   int score(String word){
     var scrabbleScore = 0;
 
-    for (var caracter = 0; caracter < word.length; caracter++) {
-      var lowerCaseCaracter = word[caracter].toLowerCase();
-      scrabbleScore += _pointsByCaracter[lowerCaseCaracter];
+    for (var character = 0; character < word.length; character++){
+      var lowerCaseCharacter = word[character].toLowerCase();
+      scrabbleScore += _pointsByCaracter[lowerCaseCharacter];
     }
 
     return scrabbleScore;
   }
-}
+  
--- scrabble-score/test/scrabble_score_test.dart
+++ scrabble-score/test/scrabble_score_test.dart
@@ -3,58 +3,55 @@ import 'package:scrabble_score/scrabble_score.dart';
 
 void main() {
   group('Scrabble score', () {
-
-    final ScrabbleScore scrabbleScore = new ScrabbleScore();
-
     group('should return a score of 0 for', () {
       test('empty input', () {
-        expect(scrabbleScore.score(''), equals(0));
+        expect(score(''), equals(0));
       }, skip: false);
     });
 
     group('should return the appropriate score for', () {
       test('lowercase letters', () {
-        expect(scrabbleScore.score('a'), equals(1));
+        expect(score('a'), equals(1));
       }, skip: false);
 
       test('uppercase letters', () {
-        expect(scrabbleScore.score('A'), equals(1));
+        expect(score('A'), equals(1));
       }, skip: false);
 
       test('valuable letters', () {
-        expect(scrabbleScore.score('f'), equals(4));
+        expect(score('f'), equals(4));
       }, skip: false);
 
       test('english-like word', () {
-        expect(scrabbleScore.score('pinata'), equals(8));
+        expect(score('pinata'), equals(8));
       }, skip: false);
 
       test('long, mixed-case word', () {
-        expect(scrabbleScore.score('OxyphenButazone'), equals(41));
+        expect(score('OxyphenButazone'), equals(41));
       }, skip: false);
 
       test('medium, valuable word', () {
-        expect(scrabbleScore.score('quirky'), equals(22));
+        expect(score('quirky'), equals(22));
       }, skip: false);
 
       test('medium word', () {
-        expect(scrabbleScore.score('street'), equals(6));
+        expect(score('street'), equals(6));
       }, skip: false);
 
       test('short, valuable word', () {
-        expect(scrabbleScore.score('zoo'), equals(12));
+        expect(score('zoo'), equals(12));
       }, skip: false);
 
       test('short word', () {
-        expect(scrabbleScore.score('at'), equals(2));
+        expect(score('at'), equals(2));
       }, skip: false);
 
       test('a word containing one of every letter of the alphabet', () {
-        expect(scrabbleScore.score('abcdefghijklmnopqrstuvwxyz'), equals(87));
+        expect(score('abcdefghijklmnopqrstuvwxyz'), equals(87));
       }, skip: false);
 
       test('a word containing one of every letter of the alphabet in reverse', () {
-        expect(scrabbleScore.score('zyxwvutsrqponmlkjihgfedcba'), equals(87));
+        expect(score('zyxwvutsrqponmlkjihgfedcba'), equals(87));
       }, skip: false);
     });
   });
