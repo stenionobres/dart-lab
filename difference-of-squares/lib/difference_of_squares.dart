8eca836bc27254c4f437c4051c34efe2203f02f9
Stenio Nobres <stenio.nobres@gmail.com>
2018-08-25 21:52:36 -0300

    An implementation for Difference of Squares problem

---
+++ difference-of-squares/lib/difference_of_squares.dart
@@ -0,0 +1,23 @@
+import 'dart:math';
+
+class DifferenceOfSquares {
+  num differenceOfSquares(int firstNaturalNumbers) =>
+    squareOfSum(firstNaturalNumbers) - sumOfSquare(firstNaturalNumbers);
+  
+  num squareOfSum(int firstNaturalNumbers){
+    int sumOfNumbers = _naturalNumbers(firstNaturalNumbers)
+                       .reduce((sum, number) => sum + number);
+
+    return pow(sumOfNumbers, 2);
+  }
+
+  num sumOfSquare(int firstNaturalNumbers){
+    num sumOfSquares = _naturalNumbers(firstNaturalNumbers)
+                       .reduce((sum, number) => sum + pow(number, 2));
+
+    return sumOfSquares;
+  }
+
+  List<num> _naturalNumbers(int firstNaturalNumbers) => 
+    List<num>.generate(firstNaturalNumbers, (i) => i + 1);
+}
