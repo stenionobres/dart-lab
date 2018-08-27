f3115c9f29f3c0cc5cc820fff6aa589a590d8adf
Stenio Nobres <stenio.nobres@gmail.com>
2018-08-27 06:08:56 -0300

    An implementation for RNA transcription

---
+++ rna-transcription/lib/rna_transcription.dart
@@ -0,0 +1,27 @@
+class RnaTranscription {
+  final Map<String, String> _rnaNucleotides = {
+    "G": "C",
+    "C": "G",
+    "T": "A",
+    "A": "U"
+  };
+
+  String toRna(String dnaStrand){
+    var rnaTranscription = "";
+
+    for (var nucleotide = 0; nucleotide < dnaStrand.length; nucleotide++) {
+      var dnaNucleotide = dnaStrand[nucleotide];
+      rnaTranscription += _getRnaNucleotide(dnaNucleotide);
+    }
+
+    return rnaTranscription;
+  }
+
+  String _getRnaNucleotide(String dnaNucleotide){
+    if(_rnaNucleotides.containsKey(dnaNucleotide))
+      return _rnaNucleotides[dnaNucleotide];
+
+    throw ArgumentError("Invalid DNA Nucleotide");
+  }
+
+}
