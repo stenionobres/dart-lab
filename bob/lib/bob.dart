07fbc786f7712a9d3e1fd8604d5655aa03ee2667
Stenio Nobres <stenio.nobres@gmail.com>
2018-09-11 19:17:12 -0300

    Refactoring according to Dart style guide
    https://www.dartlang.org/guides/language/effective-dart/style
    https://github.com/dart-lang/dart_style/wiki/Formatting-Rules

--- bob/lib/bob.dart
+++ bob/lib/bob.dart
@@ -1,24 +1,25 @@
 class Bob {
-  String hey(String message){
+  String hey(String message) {
     var cleanMessage = message.trim();
 
-    if(_isShout(cleanMessage) && _isQuestion(cleanMessage))
+    if (_isShout(cleanMessage) && _isQuestion(cleanMessage)) {
       return "Calm down, I know what I'm doing!";
+    }
     
-    if(_noMessage(cleanMessage)) return "Fine. Be that way!";
-    if(_isQuestion(cleanMessage)) return "Sure.";
-    if(_isShout(cleanMessage)) return "Whoa, chill out!";
+    if (_noMessage(cleanMessage)) return "Fine. Be that way!";
+    if (_isQuestion(cleanMessage)) return "Sure.";
+    if (_isShout(cleanMessage)) return "Whoa, chill out!";
 
     return "Whatever.";
   }
 
   bool _noMessage(String message) => message.isEmpty;
   bool _isQuestion(String message) => message.endsWith("?");
-  bool _isShout(String message){
+  bool _isShout(String message) {
     var letterRegex = new RegExp(r"[a-zA-Z]");
     var letters = letterRegex.allMatches(message);
 
-    if(letters.isEmpty) return false;
+    if (letters.isEmpty) return false;
    
     return letters.every((letter) => 
       letter.group(0) == letter.group(0).toUpperCase()
