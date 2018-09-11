2c244f10cd7f0f8a8ef14f135841036181d84535
Stenio Nobres <stenio.nobres@gmail.com>
2018-09-11 19:10:24 -0300

    Refactoring according to Dart style guide
    https://www.dartlang.org/guides/language/effective-dart/style
    https://github.com/dart-lang/dart_style/wiki/Formatting-Rules

--- difference-of-squares/lib/difference_of_squares.dart
+++ difference-of-squares/lib/difference_of_squares.dart
@@ -4,14 +4,14 @@ class DifferenceOfSquares {
   num differenceOfSquares(int firstNaturalNumbers) =>
     squareOfSum(firstNaturalNumbers) - sumOfSquare(firstNaturalNumbers);
   
-  num squareOfSum(int firstNaturalNumbers){
+  num squareOfSum(int firstNaturalNumbers) {
     int sumOfNumbers = _naturalNumbers(firstNaturalNumbers)
                        .reduce((sum, number) => sum + number);
 
     return pow(sumOfNumbers, 2);
   }
 
-  num sumOfSquare(int firstNaturalNumbers){
+  num sumOfSquare(int firstNaturalNumbers) {
     num sumOfSquares = _naturalNumbers(firstNaturalNumbers)
                        .reduce((sum, number) => sum + pow(number, 2));
 
