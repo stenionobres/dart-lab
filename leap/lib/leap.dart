71d6998fca1d973d0522f5985201348168129419
Stenio Nobres <stenio.nobres@gmail.com>
2018-09-11 19:04:16 -0300

    Refactoring according to Dart style guide
    https://www.dartlang.org/guides/language/effective-dart/style
    https://github.com/dart-lang/dart_style/wiki/Formatting-Rules

--- leap/lib/leap.dart
+++ leap/lib/leap.dart
@@ -1,23 +1,23 @@
 class Leap {
-  bool leapYear(int year){
+  bool leapYear(int year) {
     return _isLeapYear(year);
   }
 
-  bool _isLeapYear(int year){
+  bool _isLeapYear(int year) {
     return _isDivisibleByFour(year) && 
            _isNotDivisibleByHundred(year) || 
            _isDivisibleByFourHundred(year);
   }
 
-  bool _isDivisibleByFour(int year){
+  bool _isDivisibleByFour(int year) {
     return year % 4 == 0;
   }
 
-  bool _isNotDivisibleByHundred(int year){
+  bool _isNotDivisibleByHundred(int year) {
     return year % 100 > 0;
   }
 
-  bool _isDivisibleByFourHundred(int year){
+  bool _isDivisibleByFourHundred(int year) {
     return year % 400 == 0;
   }
 }
