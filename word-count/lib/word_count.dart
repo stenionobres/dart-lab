56d7965a718b81ae6545308a308c86c18b126cb7
Stenio Nobres <stenio.nobres@gmail.com>
2018-09-11 19:12:24 -0300

    Refactoring according to Dart style guide
    https://www.dartlang.org/guides/language/effective-dart/style
    https://github.com/dart-lang/dart_style/wiki/Formatting-Rules

--- word-count/lib/word_count.dart
+++ word-count/lib/word_count.dart
@@ -1,5 +1,5 @@
 class WordCount {
-  Map<String, int> countWords(String sentence){
+  Map<String, int> countWords(String sentence) {
     Map<String, int> wordCounter = new Map<String, int>();
    
     var newSentence = sentence.replaceAll(" '", ",")
@@ -14,8 +14,9 @@ class WordCount {
     var tokens = newSentence.split(",");
 
     for (var token in tokens.where((token) => token != "")) {
-      if(wordCounter.containsKey(token) == false)
+      if(wordCounter.containsKey(token) == false) {
         wordCounter[token] = 0;
+      }
 
       wordCounter[token]++;
     }
